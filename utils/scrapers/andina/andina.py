78cd20057b7fc2b856de9f6afb7e91916139f355
Renato Ramos Soto <ramossotorenato@gmail.com>
2023-06-21 14:53:50 -0500

    Libraries built

---
+++ utils/scrapers/andina/andina.py
@@ -0,0 +1,50 @@
+
+
+class Andina():
+    """
+    This class is designed to handle the ETL 
+    (Extract, Transform, Load) process.
+    """
+    def __init__(self, step = None):
+        """
+        Initialize a new instance of the CodigoCPP class.
+
+        :param step: The ETL process step to execute, 
+        which can be 'extract', 'transform', 'load', or 'all'.
+        """
+        self.load_type = step
+    
+    def extract(self):
+        """
+        Executes the extraction step of the ETL process, 
+        extracting files from SPIJ.
+        """
+        pass
+    
+    def transform(self):
+        """
+        Executes the transformation step of the ETL process, 
+        converting files into JSON format.
+        """
+        pass
+
+    def load(self):
+        """
+        Executes the load step of the ETL process, 
+        initializing Pinecone and OpenAI, and uploading data 
+        to Pinecone.
+        """
+        pass
+
+    def all(self):
+        """
+        Executes all steps of the ETL process 
+        (extract, transform, load) in sequence.
+        """
+        pass
+        
+    def run(self):
+        """
+        Runs the ETL process step specified in init
+        """
+        getattr(self, self.load_type)()
